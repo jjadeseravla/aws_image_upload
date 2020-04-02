7af1316ee1f5b7674a8e288e44db20324bdd9826
jjadeseravla <jadealvares@hotmail.co.uk>
2020-04-02 12:04:08 +0100

    Implemented the method: UserProfileService.uploadUserProfileImage(UUID userProfileId, MultipartFile file)

--- src/main/java/com/jade/awsimageupload/Profile/UserProfileService.java
+++ src/main/java/com/jade/awsimageupload/Profile/UserProfileService.java
@@ -1,20 +1,26 @@
 package com.jade.awsimageupload.Profile;
 
+import com.jade.awsimageupload.bucket.BucketName;
+import com.jade.awsimageupload.fileStore.FileStore;
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.UUID;
+import java.io.IOException;
+import java.util.*;
 
 @Service
 public class UserProfileService {
 
     private final UserProfileDataAccessService userProfileDataAccessService;
+    private final FileStore fileStore;
 
     @Autowired
-    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService,
+                              FileStore fileStore) {
         this.userProfileDataAccessService = userProfileDataAccessService;
+        this.fileStore = fileStore;
     }
 
     List<UserProfile> getUserProfiles() {
@@ -22,10 +28,37 @@ public class UserProfileService {
     }
 
     public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
-    //check if image is not empty
-        //if file is an image
+        //check if file is empty
+        if (file.isEmpty()) {
+            throw new IllegalStateException("Cannot upload empty file [ " + file.getSize() + " ]");
+        }
+
+        //if file is not an image
+        if (!Arrays.asList(ContentType.IMAGE_JPEG, ContentType.IMAGE_PNG, ContentType.IMAGE_GIF).contains(file.getContentType())) {
+            throw new IllegalStateException("File must be a JPEG, PNG or GIF to be uploaded");
+        }
+
         //check to see if user exists in db
+        UserProfile user = userProfileDataAccessService
+                .getUserProfiles()
+                .stream()
+                .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException(String.format("User profile %s not found", userProfileId)))
+
         //if yes grab metadata from file if any
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-type", file.getContentType());
+        metadata.put("Content-length", String.valueOf(file.getSize()));
+
         //store the image in s3 and update DB with s3 image link (userProfileImageLink).
+        // %s means file name
+        String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), user.getUserProfileId())
+        String filename = String.format("%s-%s", file.getName(), UUID.randomUUID());
+        try {
+            fileStore.save(path, filename, Optional.of(metadata), file.getInputStream());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
